1430fdb0fd839ff6b24e75164eafd9e1fd93a370
Stephen Gutekanst <stephen@sourcegraph.com>
2022-07-06 19:31:35 -0700

    add ECS/EC2 Container Service agent role
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- iam.tf
+++ iam.tf
@@ -1,27 +1,47 @@
-# AWS IAM roles needed to perform task execution and task management.
+# AWS IAM roles
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "${var.prefix}-ecs-task-execution-role"
-
+// ECS task execution role
+// Used for executing ECS tasks.
+resource "aws_iam_role" "ecs_task_execution" {
+  name               = "${var.prefix}-ecs-task-execution"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution.json
+}
+data "aws_iam_policy_document" "ecs_task_execution" {
   # Role policy from https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_execution_IAM_role.html
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+  version = "2012-10-17"
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
-  ]
+  }
 }
-EOF
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+
+// EC2 Container Service agent role.
+// Used for launching EC2 instances, registering them into the ECS cluster, etc.
+resource "aws_iam_role" "ecs_agent" {
+  name               = "${var.prefix}-ecs-agent"
+  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
+}
+data "aws_iam_policy_document" "ecs_agent" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role_policy_attachment" "ecs_agent" {
+  role       = aws_iam_role.ecs_agent.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
