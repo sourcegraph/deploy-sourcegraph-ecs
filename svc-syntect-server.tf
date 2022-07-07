7726f6693822cee8bdf476997b3ce8eb9a1071bc
Stephen Gutekanst <stephen@sourcegraph.com>
2022-07-06 19:01:03 -0700

    basic syntax_highlighter service
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- svc-syntect-server.tf
+++ svc-syntect-server.tf
@@ -34,27 +34,19 @@ resource "aws_ecs_task_definition" "syntax_highlighter" {
   }
 }
 
-# resource "aws_ecs_service" "syntax_highlighter" {
-#   name            = "syntax-highlighter"
-#   cluster         = aws_ecs_cluster.ecs_cluster.id
-#   task_definition = aws_ecs_task_definition.syntax_highlighter.arn
-#   desired_count   = 2
-#   iam_role        = aws_iam_role.foo.arn
-#   depends_on      = [aws_iam_role_policy.foo]
+resource "aws_ecs_service" "syntax_highlighter" {
+  name            = "syntax-highlighter"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.syntax_highlighter.arn
+  desired_count   = 2
 
-#   ordered_placement_strategy {
-#     type  = "binpack"
-#     field = "cpu"
-#   }
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
 
-#   # load_balancer {
-#   #   target_group_arn = aws_lb_target_group.foo.arn
-#   #   container_name   = "mongo"
-#   #   container_port   = 8080
-#   # }
-
-#   placement_constraints {
-#     type       = "memberOf"
-#     expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
-#   }
-# }
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b]"
+  }
+}
