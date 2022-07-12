2a1849224f7e82c3dc428d31d19153553975435f
Stephen Gutekanst <stephen@sourcegraph.com>
2022-07-12 16:42:22 -0700

    rename syntect-server -> syntax-highlighter
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- syntax-highlighter.tf
+++ syntax-highlighter.tf
@@ -30,6 +30,16 @@ resource "aws_ecs_task_definition" "syntax_highlighter" {
     }
   ])
 
+  load_balancer {
+    target_group_arn = aws_lb_target_group.foo.arn
+    container_name   = "mongo"
+    container_port   = 8080
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
   # TODO: add NLB
 }
 
