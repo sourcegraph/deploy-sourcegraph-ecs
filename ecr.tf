a60d1e9f30d8e6653a92652d25b7b19f39c035bc
Stephen Gutekanst <stephen@sourcegraph.com>
2022-07-13 19:52:18 -0700

    add indexed-search ECR repositories
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- ecr.tf
+++ ecr.tf
@@ -8,3 +8,19 @@ resource "aws_ecr_repository" "syntax_highlighter" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "search_indexer" {
+  name                 = "sourcegraph-search-indexer"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "indexed_searcher" {
+  name                 = "sourcegraph-indexed-searcher"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
--- push-sourcegraph-images.sh
+++ push-sourcegraph-images.sh
@@ -8,6 +8,14 @@ export aws_region=$(aws configure get default.region)
 docker pull sourcegraph/syntax-highlighter:3.41.0
 docker tag sourcegraph/syntax-highlighter:3.41.0 "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-syntax-highlighter:3.41.0"
 
+docker pull sourcegraph/search-indexer:3.41.0
+docker tag sourcegraph/search-indexer:3.41.0 "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-search-indexer:3.41.0"
+
+docker pull sourcegraph/indexed-searcher:3.41.0
+docker tag sourcegraph/indexed-searcher:3.41.0 "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-indexed-searcher:3.41.0"
+
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$aws_account.dkr.ecr.$aws_region.amazonaws.com"
 
 docker push "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-syntax-highlighter:3.41.0"
+docker push "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-search-indexer:3.41.0"
+docker push "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-indexed-searcher:3.41.0"
