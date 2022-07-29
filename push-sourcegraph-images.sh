ebda9c6743d8571b3eeec7b79dfd6a4319d362ea
Stephen Gutekanst <stephen@sourcegraph.com>
2022-07-28 19:22:32 -0700

    push all Docker images
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- push-sourcegraph-images.sh
+++ push-sourcegraph-images.sh
@@ -2,20 +2,45 @@
 set -exuo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
+version="3.41.0"
+
 export aws_account=$(aws sts get-caller-identity | jq -r ".Account")
 export aws_region=$(aws configure get default.region)
 
-docker pull sourcegraph/syntax-highlighter:3.41.0
-docker tag sourcegraph/syntax-highlighter:3.41.0 "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-syntax-highlighter:3.41.0"
+declare -a images=(
+    "cadvisor"
+    "codeinsights-db"
+    "codeintel-db"
+    "frontend"
+    "github-proxy"
+    "gitserver"
+    "grafana"
+    "indexed-searcher"
+    "jaeger-all-in-one"
+    "migrator"
+    "minio"
+    "postgres-12-alpine"
+    "precise-code-intel-worker"
+    "prometheus"
+    "redis-cache"
+    "redis-store"
+    "repo-updater"
+    "search-indexer"
+    "searcher"
+    "symbols"
+    "syntax-highlighter"
+    "worker"
+)
 
-docker pull sourcegraph/search-indexer:3.41.0
-docker tag sourcegraph/search-indexer:3.41.0 "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-search-indexer:3.41.0"
-
-docker pull sourcegraph/indexed-searcher:3.41.0
-docker tag sourcegraph/indexed-searcher:3.41.0 "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-indexed-searcher:3.41.0"
+for i in "${images[@]}"
+do
+    docker pull "sourcegraph/$i:$version"
+    docker tag "sourcegraph/$i:$version" "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-$i:$version"
+done
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$aws_account.dkr.ecr.$aws_region.amazonaws.com"
 
-docker push "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-syntax-highlighter:3.41.0"
-docker push "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-search-indexer:3.41.0"
-docker push "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-indexed-searcher:3.41.0"
+for i in "${images[@]}"
+do
+    docker push "$aws_account.dkr.ecr.$aws_region.amazonaws.com/sourcegraph-$i:$version"
+done
