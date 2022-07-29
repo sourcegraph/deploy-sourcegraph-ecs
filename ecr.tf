9e03272944dd1e2963d178177edc981f8206b1da
Stephen Gutekanst <stephen@sourcegraph.com>
2022-07-28 19:22:04 -0700

    ecr: create ECR repositories for all Docker images
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- ecr.tf
+++ ecr.tf
@@ -1,8 +1,136 @@
 # ECR Repositories, i.e. where Sourcegraph Docker images will be stored
 # (and scanned by ECR for security vulnerabilities.)
 
-resource "aws_ecr_repository" "syntax_highlighter" {
-  name                 = "sourcegraph-syntax-highlighter"
+resource "aws_ecr_repository" "cadvisor" {
+  name                 = "sourcegraph-cadvisor"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "codeinsights_db" {
+  name                 = "sourcegraph-codeinsights-db"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "codeintel_db" {
+  name                 = "sourcegraph-codeintel-db"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "frontend" {
+  name                 = "sourcegraph-frontend"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "github_proxy" {
+  name                 = "sourcegraph-github-proxy"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "gitserver" {
+  name                 = "sourcegraph-gitserver"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "grafana" {
+  name                 = "sourcegraph-grafana"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "indexed_searcher" {
+  name                 = "sourcegraph-indexed-searcher"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "jaeger_all_in_one" {
+  name                 = "sourcegraph-jaeger-all-in-one"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "migrator" {
+  name                 = "sourcegraph-migrator"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "minio" {
+  name                 = "sourcegraph-minio"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "postgres_12_alpine" {
+  name                 = "sourcegraph-postgres-12-alpine"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "precise_code_intel_worker" {
+  name                 = "sourcegraph-precise-code-intel-worker"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "prometheus" {
+  name                 = "sourcegraph-prometheus"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "redis_cache" {
+  name                 = "sourcegraph-redis-cache"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "redis_store" {
+  name                 = "sourcegraph-redis-store"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "repo_updater" {
+  name                 = "sourcegraph-repo-updater"
   image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
@@ -17,8 +145,32 @@ resource "aws_ecr_repository" "search_indexer" {
   }
 }
 
-resource "aws_ecr_repository" "indexed_searcher" {
-  name                 = "sourcegraph-indexed-searcher"
+resource "aws_ecr_repository" "searcher" {
+  name                 = "sourcegraph-searcher"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "symbols" {
+  name                 = "sourcegraph-symbols"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "syntax_highlighter" {
+  name                 = "sourcegraph-syntax-highlighter"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "worker" {
+  name                 = "sourcegraph-worker"
   image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
